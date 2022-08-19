189ff0d107d1f0ebaa2dc15490b004dd6c5180f8
swa07016 <swa07016@khu.ac.kr>
2022-08-20 00:23:22 +0900

    Feature: trash object 테스트

--- main.ts
+++ main.ts
@@ -11,5 +11,21 @@ let zepLogo = ScriptApp.loadSpritesheet("zep_logo.png");
 ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
 
 ScriptApp.onDestroy.Add(function () {
-  ScriptMap.clearAllObjects();
+    ScriptMap.clearAllObjects();
+});
+
+// Player
+ScriptApp.onObjectAttacked.Add(function (sender, x, y) {
+    ScriptApp.showCenterLabel(
+        `${sender.name}님이 좌표: (${x}, ${y}) 에서 쓰레기를 공격!!`
+    )
+})
+
+// Trash object
+let trash = ScriptApp.loadSpritesheet("trash.png");
+
+ScriptApp.onStart.Add(function () {
+    ScriptMap.putObject(5, 5, trash, {overlap: true});
+    ScriptMap.putObject(10, 10, trash, {overlap: true});
+    ScriptMap.putObject(20, 20, trash, {overlap: true});
 });
