b73a3d9c7642eb074b05e992c3e4843b3f708ca4
swa07016 <swa07016@khu.ac.kr>
2022-08-20 01:13:17 +0900

    Feature: 똥피하기 소스

--- main.ts
+++ main.ts
@@ -4,28 +4,284 @@
 
 import "zep-script";
 
-ScriptApp.showCenterLabel("Hello World");
+// load sprite
+let poop = ScriptApp.loadSpritesheet('poop.png', 48, 43, [0], 16);
 
-let zepLogo = ScriptApp.loadSpritesheet("zep_logo.png");
-
-ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
-
-ScriptApp.onDestroy.Add(function () {
-    ScriptMap.clearAllObjects();
+// load sprite
+let tomb = ScriptApp.loadSpritesheet('tomb.png', 32, 48, {
+    left: [0],  // defined base anim
+    right: [0], // defined base anim
+    up: [0],    // defined base anim
+    down: [0],  // defined base anim
 });
 
-// Player
-ScriptApp.onObjectAttacked.Add(function (sender, x, y) {
-    ScriptApp.showCenterLabel(
-        `${sender.name}님이 좌표: (${x}, ${y}) 에서 쓰레기를 공격!!`
-    )
-})
+const STATE_INIT = 3000;
+const STATE_READY = 3001;
+const STATE_PLAYING = 3002;
+const STATE_JUDGE = 3004;
+const STATE_END = 3005;
 
-// Trash object
-let trash = ScriptApp.loadSpritesheet("trash.png");
+let _level = 1;
+let _levelTimer = 15;
+let _levelAddTimer = 0;
 
-ScriptApp.onStart.Add(function () {
-    ScriptMap.putObject(5, 5, trash, {overlap: true});
-    ScriptMap.putObject(10, 10, trash, {overlap: true});
-    ScriptMap.putObject(20, 20, trash, {overlap: true});
+let _start = false;
+let _timer = 90;
+
+let _poops = [];
+let _stateTimer = 0;
+
+let _genTime = 0;
+let _dropTime = 0;
+
+let _live = 0;
+
+let _players = ScriptApp.players; // ScriptApp.players : get total players
+let lastSurvivor;
+
+let _state;
+let _stateTImer;
+
+function startScriptApp()
+{
+    _start = true;
+    _stateTimer = 0;
+    _genTime = 0;
+    _dropTime = 0;
+    _timer = 90;
+
+    for(let i in _players) {
+        let p = _players[i];
+        // create and utilize option data using tags.
+        p.tag = {
+            alive : true,
+        };
+    }
+}
+
+function startState(state)
+{
+    _state = state;
+    _stateTImer = 0;
+    switch(_state)
+    {
+        case STATE_INIT:
+            startScriptApp();
+            break;
+        case STATE_READY:
+            break;
+        case STATE_PLAYING:
+            // Show Label
+            ScriptApp.showCenterLabel("Game Start");
+            break;
+        case STATE_JUDGE:
+            for(let i in _poops) {
+                let b = _poops[i];
+                ScriptMap.putObject(b[0], b[1], null);
+            }
+            break;
+        case STATE_END:
+            _start = false;
+            for(let i in _players) {
+                let p = _players[i];
+                p.sprite = null;
+                p.moveSpeed = 80;
+                p.sendUpdated();
+            }
+            break;
+    }
+}
+
+function checkSuvivors() {
+    if(!_start)
+        return;
+
+    let alive = 0;
+    for(let i in _players) {
+        let p = _players[i];
+        if(!p.sprite) {
+            lastSurvivor = p;
+            ++alive;
+        }
+    }
+
+    return alive;
+}
+
+ScriptApp.onStart.Add(function() {
+    startState(STATE_INIT);
+});
+
+// when player join the space event
+// 플레이어가 스페이스에 입장 했을 때 이벤트
+ScriptApp.onJoinPlayer.Add(function(p) {
+    // create and utilize option data using tags.
+    if(_start)
+    {
+        p.tag = {
+            alive : false,
+        };
+
+        // change move speed
+        p.moveSpeed = 20;
+        // change sprite image
+        p.sprite = tomb;
+        // when player property changed have to call this method
+        // 플레이어 속성 변경 시 반드시 호출하여 업데이트 한다.
+        p.sendUpdated();
+    }
+    _players = ScriptApp.players;
+});
+
+// when player leave the space event
+// 플레이어가 스페이스를 나갔을 때 이벤트
+ScriptApp.onLeavePlayer.Add(function(p) {
+    p.title = null;
+    p.sprite = null;
+    p.moveSpeed = 80;
+    p.sendUpdated();
+
+    _players = ScriptApp.players; // ScriptApp.players : get total players
+});
+
+// when player touched objects event
+// 플레이어가 오브젝트와 부딪혔을 때
+ScriptApp.onObjectTouched.Add(function(sender, x, y, tileID) {
+    if(!_start)
+        return;
+
+    if(!sender.tag.alive)
+        return;
+
+    sender.tag.alive = false;
+    sender.sprite = tomb;
+    sender.moveSpeed = 40;
+    sender.sendUpdated();
+
+    _live = checkSuvivors();
+
+    if(_live == 1 || _live == 0)
+    {
+        startState(STATE_JUDGE);
+    }
+    else
+    {
+        if(_stateTimer >= 1)
+        {
+            _stateTimer = 0;
+            _timer--;
+            if(_timer <= 0)
+            {
+                startState(STATE_JUDGE);
+            }
+        }
+    }
+});
+
+// when the game block is pressed event
+// 게임 블록을 밟았을 때 호출되는 이벤트
+ScriptApp.onDestroy.Add(function() {
+    for(let i in _poops) {
+        let b = _poops[i];
+        ScriptMap.putObject(b[0], b[1], null);
+    }
+});
+
+// called every 20ms
+// 20ms 마다 호출되는 업데이트
+// param1 : deltatime ( elapsedTime )
+ScriptApp.onUpdate.Add(function(dt) {
+    if(!_start)
+        return;
+
+    _stateTimer += dt;
+    switch(_state)
+    {
+        case STATE_INIT:
+            ScriptApp.showCenterLabel(`Avoid falling poop.`);
+
+            if(_stateTimer >= 5)
+            {
+                startState(STATE_READY);
+            }
+            break;
+        case STATE_READY:
+            ScriptApp.showCenterLabel(`The game will start soon.`);
+
+            if(_stateTimer >= 3)
+            {
+                startState(STATE_PLAYING);
+            }
+            break;
+        case STATE_PLAYING:
+            _genTime -= dt;
+            if(_genTime <= 0) {
+                _genTime = Math.random() * (0.5 - (_level * 0.05));
+
+                let b = [Math.floor(ScriptMap.width * Math.random()),-1];
+
+                _poops.push(b);
+                if(b[1] >= 0)
+                    ScriptMap.putObject(b[0], b[1], poop, {
+                        overlap: true,
+                    });
+            }
+
+            _dropTime -= dt;
+            if(_dropTime <= 0) {
+                _dropTime = Math.random() * (0.5 - (_level * 0.08));
+
+                for(let i in _poops) {
+                    let b = _poops[i];
+                    ScriptMap.putObject(b[0], b[1], null);
+
+                    b[1]++;
+                    if(b[1] < ScriptMap.height) {
+                        ScriptMap.putObject(b[0], b[1], poop, {
+                            overlap: true,
+                        });
+                    }
+                }
+
+                for(let k = _poops.length - 1;k >= 0;--k) {
+                    let b = _poops[k];
+                    if(b[1] >= ScriptMap.height)
+                        _poops.splice(k, 1);
+                }
+            }
+
+            _levelAddTimer += dt;
+            if(_levelAddTimer >= _levelTimer)
+            {
+                _level++;
+                _levelAddTimer = 0;
+
+                if(_level > 6)
+                {
+                    _level = 6;
+                }
+            }
+            break;
+        case STATE_JUDGE:
+            if(_live == 1)
+            {
+                ScriptApp.showCenterLabel(`${lastSurvivor.name} is last suvivor`);
+            }
+            else if(_live == 0)
+            {
+                ScriptApp.showCenterLabel(`There are no survivors.`);
+            }
+            else
+            {
+                ScriptApp.showCenterLabel(`Final survivors : ` + _live);
+            }
+
+            if(_stateTimer >= 5)
+            {
+                startState(STATE_END);
+            }
+            break;
+        case STATE_END:
+            break;
+    }
 });
